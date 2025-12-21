4411a4a781abb49a0f5c2dc31ac30dbcc0450751
Muzammil ahmed <muzammilofficial49@gmail.com>
2025-12-21 13:10:57 +0500

    updates

--- config.js
+++ config.js
@@ -4,13 +4,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.5/firebas
 import {  getFirestore, setDoc, deleteField, updateDoc, getDocs, doc, getDoc, collection, query, where, arrayUnion, arrayRemove 
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js"; 
   const firebaseConfig = {
-    apiKey: "AIzaSyDufjkWy3sdnv4b7GWNEOGomVb10nbns0A",
-    authDomain: "chat-app-14245.firebaseapp.com",
-    projectId: "chat-app-14245",
-    storageBucket: "chat-app-14245.firebasestorage.app",
-    messagingSenderId: "869254175172",
-    appId: "1:869254175172:web:9deaa647909f5f7d36ec91",
-    measurementId: "G-HMLRSBW49K"
+    
   };
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
